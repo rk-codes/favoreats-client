6f7c63d242cc356bdbad28d22ce8debdd4dc564f
rk-codes <ping.you.back@gmail.com>
2018-03-08 15:03:55 -0800

    reorder components

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import Header from './header';
 import RestaurantsList from './restaurants-list';
 
 export default function HomePage() {
     return(
         <div>
+            <Header />
             <h1>Home Page</h1>
             <RestaurantsList />
+            <button>Add Restaurant</button>
         </div>
     )
 }
