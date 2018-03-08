1a2f20d8b0da67f44f3cbfea5d11bae9dea2a9ae
rk-codes <ping.you.back@gmail.com>
2018-03-08 15:02:16 -0800

    add forms

--- src/components/add-dish-form.js
+++ src/components/add-dish-form.js
@@ -4,7 +4,17 @@ import React from 'react';
 export default function AddDishForm(){
     return(
         <form>
-            <legend>Add dish form</legend>
+            <fieldset>
+                <legend>Add dish form</legend>
+                <label htmlFor="name">Dish Name</label>
+                <input type="text" />
+                <label htmlFor="name">Rating</label>
+                <input type="text" />
+                <label htmlFor="name">Review</label>
+                <input type="text" />
+                <button>Cancel</button>
+                <button type="submit">Add</button>
+            </fieldset>     
         </form>
     )
 }
--- src/components/add-restaurant-form.js
+++ src/components/add-restaurant-form.js
@@ -3,8 +3,18 @@ import React from 'react';
 
 export default function AddRestaurantForm(){
     return(
-        <form>
-            <legend>Add restaurant form</legend>
+        <form className="add-restaurant-form">
+            <fieldset>
+                <legend>Add restaurant form</legend>
+                <label htmlFor="name">Restaurant Name</label>
+                <input type="text" />
+                <label htmlFor="name">City/State</label>
+                <input type="text" />
+                <label htmlFor="name">Cuisine</label>
+                <input type="text" />
+                <button>Cancel</button>
+                <button type="submit">Add</button>
+            </fieldset>     
         </form>
     )
 }
