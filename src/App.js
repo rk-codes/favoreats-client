47d7831cc749e6a737992d9ff331859a86312eaf
rk-codes <ping.you.back@gmail.com>
2018-03-08 11:09:58 -0800

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
-import {connect} from 'react-redux';
+
 import {BrowserRouter as Router} from 'react-router-dom';
 
 
