b026a3ed5d18dab925533f6c6789ce2dd61a5cb6
rk-codes <ping.you.back@gmail.com>
2018-03-08 18:32:02 -0800

    add state and connect

--- src/components/restaurant-info.js
+++ src/components/restaurant-info.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 export default function RestaurantInfo(props) {
     return(
         <div className="restaurant-info">
-            <Link to= {`/restaurant/${props.id}/dishes`}>Restaurant Name: {props.name}</Link>
+            <Link to= {`/restaurants/${props.id}/dishes`}>Restaurant Name: {props.name}</Link>
             <p>Location: {props.location}</p>
             <span>Cuisine: {props.cuisine}</span>
             <p>Dishes Count: {props.dishCount}</p>
--- src/components/restaurants-list.js
+++ src/components/restaurants-list.js
@@ -1,37 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import RestaurantInfo from './restaurant-info';
 
-export default function RestaurantsList() {
+class  RestaurantsList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            restaurants: [{
+                id: '1',
+                name: 'ABC',
+                location: 'San Francisco',
+                cuisine: 'American',
+                dishCount: '3' 
+            },
+            {
+                id: '2',
+                name: 'BCD',
+                location: 'Las Vegas',
+                cuisine: 'Mexican',
+                dishCount: '1' 
+            }]
+        }
+    }
 
-    const data = [{
-        id: '1',
-        name: 'ABC',
-        location: 'San Francisco',
-        cuisine: 'American',
-        dishCount: '3' 
-    },
-    {
-        id: '2',
-        name: 'BCD',
-        location: 'Las Vegas',
-        cuisine: 'Mexican',
-        dishCount: '1' 
-    }]
+    render() {
+        const info = this.state.restaurants.map((item, index) =>
+            <li key={index}>
+                <RestaurantInfo {...item} />
+            </li>
+        );
 
-    const info = data.map((item, index) =>
-        <li key={index}>
-            <RestaurantInfo {...item} />
-        </li>
-    );
-
-    return(    
-        <div className="restaurants-list">
-            <h1>Restaurants List</h1>
-            <ul>
-                {info}
-            </ul>
-            
-        </div>
-    )
+        return(    
+            <div className="restaurants-list">
+                <h1>Restaurants List</h1>
+                <ul>
+                    {info}
+                </ul>
+                <button>Add Restaurant</button>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = (state) => {
+    return{
+        restaurants: {...state.restaurants}
+    }
+}
+export default connect(mapStateToProps)(RestaurantsList);
