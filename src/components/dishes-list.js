f6e938ec54e9f1a0f77b8a50a00eab4c1e5be248
rk-codes <ping.you.back@gmail.com>
2018-03-08 19:09:57 -0800

    add state and connect

--- src/components/dishes-list.js
+++ src/components/dishes-list.js
@@ -1,34 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import DishInfo from './dish-info';
 
-export default function DishesList() {
-    const data = [{
-        dishId: '1',
-        name: 'Xyz',
-        rating: '3'
-    },
-    {
-        dishId: '2',
-        name: 'Bcd',
-        rating: '5'
+class DishesList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: [{
+                dishId: '1',
+                name: 'Xyz',
+                rating: '3'
+            },
+            {
+                dishId: '2',
+                name: 'Bcd',
+                rating: '5'
+            }]
+        }
     }
-    ]
 
-    const info = data.map((item, index) =>
+    render() {
+        const info = this.state.dishes.map((item, index) =>
         <li key={index}>
             <DishInfo {...item} />
         </li>
     )
-    return(
-        <div className="dishes-list">
-            <h3>Restaurant Name</h3>
-            <p>Cuisine</p>
-            <span>Dishes count</span>
-            <ul>
-                {info}
-            </ul>
-            <button>Add dish</button>
-        </div>
-    )
+        return(
+            <div className="dishes-list">
+                <h3>Restaurant Name</h3>
+                <p>Cuisine</p>
+                <span>Dishes count</span>
+                <ul>
+                    {info}
+                </ul>
+                <button>Add dish</button>
+            </div>
+        )
+    }
+   
 }
+const mapStateToProps = (state) => {
+    return{
+        dishes: {...state.dishes}
+    }
+}
+
+export default connect(mapStateToProps)(DishesList);
