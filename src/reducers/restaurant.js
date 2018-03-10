c98c5487da3ed92a080e2c52783d7a5c59816adb
rk-codes <ping.you.back@gmail.com>
2018-03-10 01:35:52 -0800

    add switch cases for actions

--- src/reducers/restaurant.js
+++ src/reducers/restaurant.js
@@ -1,20 +1,40 @@
 import * as actions from '../actions';
 
 const initialState = {
-    restaurants: []
+    restaurants: [{
+        dishes: []
+    }]
 }
 export default  (state=initialState, action) => {
     console.log(action);
     switch(action.type) {
-        case actions.ADD_RESTAURANT:
-         return(Object.assign({}, state, {
-             restaurants: [...state.restaurants, action.payload] 
-         }));
+        case actions.ADD_RESTAURANT_SUCCESS:
+            return(Object.assign({}, state, {
+                restaurants: [...state.restaurants, action.payload] 
+            }));
      
         case actions.FETCH_ALL_RESTAURANTS_SUCCESS: 
             return(Object.assign({}, state, {
-                    restaurants: action.payload 
-                 }));
+                restaurants: action.payload 
+            }));
+        case actions.FETCH_ALL_DISHES_SUCCESS: 
+                let restaurant = state.restaurants[0];
+                console.log(restaurant);
+                //return Object.assign({}, restaurant,{dishes: action.payload})
+            
+            // return(Object.assign({}, state,  { 
+            //         dishes: action.payload
+            //      })
+               
+            // );
+        case actions.DELETE_RESTAURANT_SUCCESS:
+        case actions.EDIT_RESTAURANT_SUCCESS:
+        case actions: FETCH_ALL_DISHES_SUCCESS;
+        case actions.ADD_DISH_SUCCESS:
+        case actions.DELETE_DISH_SUCCESS:
+        case actions.EDIT_DISH_SUCCESS:
+        case actions.ADD_DISH_REVIEW_SUCCESS:
+        case actions.FETCH_ALL_REVIEWS_SUCCESS:
         default: return state;
     }
 
