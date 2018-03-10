895bb88d8a266f3e3325034e0735426a2ccc374d
rk-codes <ping.you.back@gmail.com>
2018-03-10 01:20:18 -0800

    add dispatch to edit and delete

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const fetchAllRestaurantsSuccess = (data) => ({
 export const FETCH_ALL_RESTAURANTS_ERROR = 'FETCH_ALL_RESTAURANTS_ERROR';
 export const fetchAllRestaurantsError = (error) => ({
     type: FETCH_ALL_RESTAURANTS_ERROR,
-    error
+    payload: error
 })
 
 //Get all restaurants from db
@@ -44,37 +44,97 @@ export const fetchAllRestaurants = () => dispatch => {
     }, 300);
 }
    
-     
-
-
 //Add a new restaurant
-export const ADD_RESTAURANT = 'ADD_RESTAURANT';
-export const addRestaurant = (restaurant) => ({
+export const ADD_RESTAURANT_SUCCESS = 'ADD_RESTAURANT_SUCCESS';
+export const addRestaurantSuccess = (data) => ({
+    type: ADD_RESTAURANT_SUCCESS,
+    payload: data
+})
+export const ADD_RESTAURANT_ERROR = 'ADD_RESTAURANT_ERROR';
+export const addRestaurantError = (error) => ({
+    type: ADD_RESTAURANT_ERROR,
+    payload: error
+})
+export const addRestaurant = () => (dispatch) => {
    //API call to POST
    // fetch(`${API_BASE_URL/restaurants}`)
-    type: ADD_RESTAURANT,
-    payload: restaurant
-})
+   const restaurant =  {
+        id: '1',
+        name: 'ABC',
+        location: 'San Francisco',
+        cuisine: 'Italian',
+        dishCount: '0'
+    }
+
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(addRestaurantSuccess(restaurant))
+    }, 300);
+}
 
 //Delete a restaurant
-export const deleteRestaurant = (restaurant) => ({
+export const DELETE_RESTAURANT_SUCCESS = 'DELETE_RESTAURANT_SUCCESS';
+export const deleteRestaurantSuccess = (data) => ({
+    type: DELETE_RESTAURANT_SUCCESS,
+    payload: data
+})
+export const DELETE_RESTAURANT_ERROR = 'DELETE_RESTAURANT_ERROR';
+export const deleteRestaurantError = (error) => ({
+    type: DELETE_RESTAURANT_ERROR,
+    payload: error
+})
+export const deleteRestaurant = () => (dispatch) => {
    //API call to DELETE
    // fetch(`${API_BASE_URL/restaurants/:restaurantId}`)
-})
+   const restaurant =  {
+        id: '1',
+        name: 'ABC',
+        location: 'San Francisco',
+        cuisine: 'Italian',
+        dishCount: '2'
+    }
+
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(deleteRestaurantSuccess(restaurant))
+    }, 300);
+}
 
 //Edit a restaurant
-export const editRestaurant = (restaurant) => ({
+export const EDIT_RESTAURANT_SUCCESS = 'EDIT_RESTAURANT_SUCCESS';
+export const editRestaurantSuccess = (data) => ({
+    type: EDIT_RESTAURANT_SUCCESS,
+    payload: data
+})
+export const EDIT_RESTAURANT_ERROR = 'EDIT_RESTAURANT_ERROR';
+export const editRestaurantError = (error) => ({
+    type: EDIT_RESTAURANT_ERROR,
+    payload: error
+})
+export const editRestaurant = () => (dispatch) => {
     //API call to PUT
      // fetch(`${API_BASE_URL/restaurants/:restaurantId}`)
- })
+     const restaurant =  {
+        id: '1',
+        name: 'XYY',
+        location: 'San Francisco',
+        cuisine: 'Italian',
+        dishCount: '2'
+    }
+
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(editRestaurantSuccess(restaurant))
+    }, 300);
+ }
 
 
  //Actions for managing dishes
 
- export const FETCH_ALL_DISHES_SUCCESS = 'FETCH_ALL_DISHES_SUCCESS';
-export const fetchAllDishesSuccess = (data) => ({
+export const FETCH_ALL_DISHES_SUCCESS = 'FETCH_ALL_DISHES_SUCCESS';
+export const fetchAllDishesSuccess = (dishes) => ({
     type: FETCH_ALL_DISHES_SUCCESS,
-    data
+    payload: dishes
 })
 export const FETCH_ALL_DISHES_ERROR = 'FETCH_ALL_DISHES_ERROR';
 export const fetchAllDishesError = (error) => ({
@@ -83,36 +143,156 @@ export const fetchAllDishesError = (error) => ({
 })
 
 //Get all dishes of a restaurant from db
-export const fetchAllDishes = (dispatch) => ({
+export const fetchAllDishes = () => dispatch =>{
     //API call to GET
     // fetch(`${API_BASE_URL/restaurants/:restaurantId/dishes}`)
-})
+    console.log("fetach all");
+   
+       const dishes = [{
+            dishId: '1',
+            name: 'Xyz',
+            rating: '3'
+        },
+        {
+            dishId: '2',
+            name: 'Bcd',
+            rating: '5'
+        }]
+        
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(fetchAllDishesSuccess(dishes))
+    }, 300);
+}
+
 //Add a new dish
-export const addDish = (dish) => ({
+export const ADD_DIS_REVIEW_SUCCESS = 'ADD_DIS_REVIEW_SUCCESS';
+export const addDishSuccess = (data) => ({
+    type: ADD_DIS_REVIEW_SUCCESS,
+    payload: data
+})
+export const ADD_DISH_ERROR = 'ADD_DISH_ERROR';
+export const addDishError = (error) => ({
+    type: ADD_DISH_ERROR,
+    payload: error
+})
+export const addDish = () => (dispatch) => {
     //API call to POST
     // fetch(`${API_BASE_URL/restaurants/:restaurantId/dishes}`)
- })
+    const dish = {
+        dishId: '3',
+        name: 'aab',
+        rating: '3'
+    }
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(addDishSuccess(dish))
+    }, 300);
+
+ }
  
  //Delete a dish
- export const deleteDish = (dish) => ({
+export const DELETE_DISH_SUCCESS = 'DELETE_DISH_SUCCESS';
+export const deleteDishSuccess = (data) => ({
+    type: DELETE_DISH_SUCCESS,
+    payload: data
+})
+export const DELETE_DISH_ERROR = 'DELETE_DISH_ERROR';
+export const deleteDishError = (error) => ({
+    type: DELETE_DISH_ERROR,
+    payload: error
+})
+ export const deleteDish = () => (dispatch) => {
     //API call to DELETE
     // fetch(`${API_BASE_URL/restaurants/:restaurantId/dishes/:dishId}`)
- })
+    const dish = {
+        dishId: '3',
+        name: 'aab',
+        rating: '3'
+    }
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(deleteDishSuccess(dish))
+    }, 300);
+ }
  
  //Edit a dish
- export const editDish = (dish) => ({
+ export const EDIT_DISH_SUCCESS = 'EDIT_DISH_SUCCESS';
+ export const editDishSuccess = (data) => ({
+     type: EDIT_DISH_SUCCESS,
+     payload: data
+ })
+ export const EDIT_DISH_ERROR = 'EDIT_DISH_ERROR';
+ export const editDishError = (error) => ({
+     type: EDIT_DISH_ERROR,
+     payload: error
+ })
+ export const editDish = () => (dispatch) => {
      //API call to PUT
       // fetch(`${API_BASE_URL/restaurants/:restaurantId/dishes/:dishId}`)
-  })
+      const dish = {
+        dishId: '3',
+        name: 'aab',
+        rating: '3'
+    }
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(editDishSuccess(dish))
+    }, 300);
+  }
  
   //Add review to a dish
-  export const addDishReview = (review) => ({
+  export const ADD_DISH_REVIEW_SUCCESS = 'ADD_DISH_REVIEW_SUCCESS';
+  export const addDishReviewSuccess = (data) => ({
+      type: ADD_DISH_REVIEW_SUCCESS,
+      payload: data
+  })
+  export const ADD_DISH_REVIEW_ERROR = 'ADD_DISH_REVIEW_ERROR';
+  export const addDishReviewError = (error) => ({
+      type: ADD_DISH_REVIEW_ERROR,
+      payload: error
+  })
+  export const addDishReview = () => (dispatch) => {
     //API call to POST
      // fetch(`${API_BASE_URL/restaurants/:restaurantId/dishes/:dishId/addreview}`)
- })
+     const review = { 
+        date: '4/5/2017',
+        rating: '3',
+        review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    }
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(addDishReviewSuccess(review))
+    }, 300);
+
+ }
 
  //Get all reviews of a dish
- export const fetchAllReviewsOfDish = (dispatch) => ({
+ export const FETCH_ALL_REVIEWS_SUCCESS = 'FETCH_ALL_REVIEWS_SUCCESS';
+export const fetchAllReviewsSuccess = (reviews) => ({
+    type: FETCH_ALL_REVIEWS_SUCCESS,
+    payload: reviews
+})
+export const FETCH_ALL_REVIEWS_ERROR = 'FETCH_ALL_REVIEWS_ERROR';
+export const fetchAllReviewsError = (error) => ({
+    type: FETCH_ALL_REVIEWS_ERROR,
+    error
+})
+ export const fetchAllReviewsOfDish = () => (dispatch) => {
     //API call to GET
     // fetch(`${API_BASE_URL/restaurants/:restaurantId/dishes/:dishId/reviews}`)
-})
+    const reviews=[{
+        date: '4/5/2017',
+        rating: '3',
+        review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+        date: '5/6/2017',
+        rating: '4',
+        review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    }]
+    setTimeout(() => { 
+        console.log("success");
+        dispatch(fetchAllReviewsSuccess(reviews))
+    }, 300);
+}
