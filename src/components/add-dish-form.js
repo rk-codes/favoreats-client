f2204b6b10fef3d1abf5e345c05a7d61a5749aa8
rk-codes <ping.you.back@gmail.com>
2018-03-11 00:30:32 -0800

    add success and error messages

--- src/components/add-dish-form.js
+++ src/components/add-dish-form.js
@@ -10,18 +10,35 @@ export class AddDishForm extends React.Component{
         //API call to POST new restaurant
         dispatch(addDish(values));
     }
-
+    
     render() {
+        let successMessage;
+        if (this.props.submitSucceeded) {
+            successMessage = (
+                <div className="message message-success">
+                    Message submitted successfully
+                </div>
+            );
+        }
+
+        let errorMessage;
+        if (this.props.error) {
+            errorMessage = (
+                <div className="message message-error">{this.props.error}</div>
+            );
+        }
         return(
             <form className="add-dish-form" onSubmit={this.props.handleSubmit((values, dispatch) => this.onSubmit(values, dispatch))}>
+                {successMessage}
+                {errorMessage}
                 <fieldset>
                     <legend>Add dish form</legend>
                     <label htmlFor="name">Dish Name</label>
                     <Field name="name" type="text" component="input" />
-                    <label htmlFor="name">Rating</label>
-                    <Field name="name" type="text" component="input" />
-                    <label htmlFor="name">Review</label>
-                    <Field name="name" type="text" component="input" />
+                    <label htmlFor="rating">Rating</label>
+                    <Field name="rating" type="text" component="input" />
+                    <label htmlFor="review">Review</label>
+                    <Field name="review" type="text" component="input" />
                     <button>Cancel</button>
                     <button type="submit">Add</button>
                 </fieldset>     
