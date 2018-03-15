32f26bb990d4a3cff088263c38841b6588e7d401
rk-codes <ping.you.back@gmail.com>
2018-03-15 00:22:26 -0700

    change component to class

--- src/components/dish-info.js
+++ src/components/dish-info.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import {deleteDish} from '../actions';
 
-export default function DishInfo(props) {
-     
-    return(
-        <div className="dish-info">
-            <h3>DishInfo</h3>
-            <p>Dish Name: {props.name}</p>
-            <p>Rating: {props.rating}</p>
-           
-            <Link to={`/restaurants/1/dishes/${props.dishId}/addreview`}>Add review</Link>
-            <Link to={`/restaurants/1/dishes/${props.dishId}/reviews`}>View past reviews</Link>
-        </div>
-    )
+
+export  class DishInfo extends React.Component{
+    render() {
+        return(
+            <div className="dish-info">
+                <h3>DishInfo</h3>
+                <p>Dish Name: {this.props.name}</p>
+                <p>Rating: {this.props.rating}</p>
+                <button onClick={() => this.onDelete()}>Delete</button>
+               
+                <Link to={`/restaurants/1/dishes/${this.props.dishId}/addreview`}>Add review</Link>
+                <Link to={`/restaurants/1/dishes/${this.props.dishId}/reviews`}>View past reviews</Link>
+            </div>
+        )
+    }  
 }
+const mapStateToProps = (state) => {
+    return{
+        restaurants: state.restaurant.restaurants,
+        dishes: state.restaurant.dishes
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        deleteDish: () => dispatch(deleteDish()),
+    })
+}
+export default connect(mapStateToProps, mapDispatchToProps)(DishInfo);
