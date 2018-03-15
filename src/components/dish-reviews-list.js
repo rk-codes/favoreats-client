13c09357070ce1d3fdd76279f67da84341a2465c
rk-codes <ping.you.back@gmail.com>
2018-03-15 15:46:56 -0700

    change to class component

--- src/components/dish-reviews-list.js
+++ src/components/dish-reviews-list.js
@@ -1,30 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import DishReview from './dish-review';
+import store from '../store';
+import { fetchAllReviewsOfDish } from '../actions';
 
-export default function DishReviewsList(props) {
-    const reviews=[{
-        date: '4/5/2017',
-        rating: '3',
-        review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-        date: '5/6/2017',
-        rating: '4',
-        review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+export  class DishReviewsList extends React.Component{
+
+    componentDidMount() {
+        this.props.fetchAllReviewsOfDish();
     }
-    ]
-
-    const dishReviews = reviews.map((item, index) =>
-        <li key={index}>
-            <DishReview {...item} />
-        </li>
-    )
-    return(
-        <div className="reviews">
-        <h3>Dish Name</h3>
-            <ul>
-                {dishReviews}
-            </ul>
-        </div>
-    )
+    render() {
+        console.log("DishReviewsList")
+        console.log(store.getState())
+        const reviews=[{
+            date: '4/5/2017',
+            rating: '3',
+            review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        },
+        {
+            date: '5/6/2017',
+            rating: '4',
+            review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        }
+        ]
+    
+        const dishReviews = reviews.map((item, index) =>
+            <li key={index}>
+                <DishReview {...item} />
+            </li>
+        )
+        return(
+            <div className="reviews">
+            <h3>Dish Name</h3>
+                <ul>
+                    {dishReviews}
+                </ul>
+            </div>
+        )
+    }  
 }
+const mapStateToProps = (state) => {
+    return{
+        restaurants: state.restaurant.restaurants,
+        dishes: state.restaurant.dishes
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        fetchAllReviewsOfDish: () => dispatch(fetchAllReviewsOfDish()),
+    })
+}
+export default connect(mapStateToProps, mapDispatchToProps)(DishReviewsList);
+
