7c36b795e61d678d73083cb17fef4c6377b972f4
rk-codes <ping.you.back@gmail.com>
2018-03-17 10:01:19 -0700

    comment dispatch to set and refresh auth token

--- src/store.js
+++ src/store.js
@@ -10,11 +10,11 @@ const store = createStore(
 );
 
 // Hydrate the authToken from localStorage if it exist
-const authToken = loadAuthToken();
-if (authToken) {
-    const token = authToken;
-    store.dispatch(setAuthToken(token));
-    store.dispatch(refreshAuthToken());
-}
+// const authToken = loadAuthToken();
+// if (authToken) {
+//     const token = authToken;
+//     store.dispatch(setAuthToken(token));
+//     store.dispatch(refreshAuthToken());
+// }
 
 export default store;
