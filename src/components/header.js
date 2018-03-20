86456d3afd8de7f3281c807657fa44973f1ff4e1
rk-codes <ping.you.back@gmail.com>
2018-03-20 01:47:14 -0700

    update nav links for logged in user

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { clearAuth } from "../actions/auth";
+import { clearAuthToken } from "../local-storage";
 
-export default function Header(){
-    return(
-        <div className="header">
-            <h1>FavorEats</h1>
-            <nav>
+export class Header extends React.Component{
+
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
+        // if (this.props.hasAuthToken === false) {
+        //     return <Redirect to="/" />;
+        // }
+    }
+    render() {
+        let navLinks;
+
+        if(this.props.loggedIn) {
+            console.log("logged in");
+            navLinks = (
                 <ul>
-                    <li><a href="/signup">Sign Up</a></li>
-                    <li><a href="/login">Login</a></li>
+                    <li><Link to = '/home'>Dashboard</Link></li>
+                    <li><Link to = '/addrestaurant'>Add Restaurant</Link></li>
+                    <li><a href="" onClick={() => this.logOut()}>Logout</a></li>
                 </ul>
-            </nav>
-        </div>
-    )
+            )
+        }
+        else {
+            navLinks = (
+                <ul>
+                    <li><Link to = "/signup">Sign Up</Link></li>
+                    <li><Link to = "/login">Login</Link></li>
+                    
+                </ul>
+            )
+        }
+        return(
+            <div className="header">
+                <h1>FavorEats</h1>
+                <nav>
+                    {navLinks}
+                </nav>
+            </div>
+        )
+    }
+    
 }
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+  
+  export default connect(mapStateToProps)(Header);
