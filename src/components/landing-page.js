a1790966777355f905a1c1973ab93bab7b8c59ed
rk-codes <ping.you.back@gmail.com>
2018-03-19 18:33:52 -0700

    add redirect to user home page for logged in user

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
 
 import IntroSection from './intro-section';
 
-export default function LandingPage() {
+export function LandingPage(props) {
+    // If we are logged in redirect straight to the user's dashboard
+    if (props.loggedIn) {
+        return <Redirect to="/home" />;
+    }
     return(
         <div>
             <IntroSection />
         </div>
     )
 }
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(LandingPage);
