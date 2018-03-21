6049b5467f21cd4e36d5a556430cad190bc66b8d
rk-codes <ping.you.back@gmail.com>
2018-03-21 01:48:24 -0700

    update props key names

--- src/components/dish-review.js
+++ src/components/dish-review.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function DishReview(props) {
+    console.log(props);
     return(
         <div className="dish-review">
-            <p>Date: {props.date}</p>
+            <p>Date: {props.reviewDate}</p>
             <span>Rating: {props.rating}</span>
-            <p>Review: {props.review}</p>
+            <p>Review: {props.description}</p>
         </div>
     )
 }
