27c13509c30298ffd936d47d961178131ab5ab3a
rk-codes <ping.you.back@gmail.com>
2018-03-21 17:50:44 -0700

    handle cancel button click

--- src/components/add-dish-form.js
+++ src/components/add-dish-form.js
@@ -1,53 +1,61 @@
-import React from 'react';
-import {reduxForm, Field} from 'redux-form';
-import { addDish } from '../actions';
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+import { addDish } from "../actions";
 
-export class AddDishForm extends React.Component{
-
-    onSubmit(values, dispatch){     
-        console.log("Dish Form submitted");
-        console.log(values);
-        //API call to POST new restaurant
-        dispatch(addDish(this.props.match.params.restaurantId, values));
-        this.props.history.push(`/restaurants/${this.props.match.params.restaurantId}/dishes`);
+export class AddDishForm extends React.Component {
+  onSubmit(values, dispatch) {
+    dispatch(addDish(this.props.match.params.restaurantId, values));
+    this.props.history.push(
+      `/restaurants/${this.props.match.params.restaurantId}/dishes`
+    );
+  }
+  onCancelClick() {
+    this.props.history.push(
+      `/restaurants/${this.props.match.params.restaurantId}/dishes`
+    );
+  }
+  render() {
+    let successMessage;
+    if (this.props.submitSucceeded) {
+      successMessage = (
+        <div className="message message-success">
+          Message submitted successfully
+        </div>
+      );
     }
-    
-    render() {
-        console.log(this.props);
-        let successMessage;
-        if (this.props.submitSucceeded) {
-            successMessage = (
-                <div className="message message-success">
-                    Message submitted successfully
-                </div>
-            );
-        }
 
-        let errorMessage;
-        if (this.props.error) {
-            errorMessage = (
-                <div className="message message-error">{this.props.error}</div>
-            );
-        }
-        return(
-            <form className="add-dish-form" onSubmit={this.props.handleSubmit((values, dispatch) => this.onSubmit(values, dispatch))}>
-                {successMessage}
-                {errorMessage}
-                <fieldset>
-                    <legend>Add dish form</legend>
-                    <label htmlFor="name">Dish Name</label>
-                    <Field name="name" type="text" component="input" />
-                    <label htmlFor="rating">Rating</label>
-                    <Field name="rating" type="text" component="input" />
-                    <label htmlFor="description">Review</label>
-                    <Field name="description" type="text" component="input" />
-                    <button>Cancel</button>
-                    <button type="submit">Add</button>
-                </fieldset>     
-            </form>
-        )
-    }    
+    let errorMessage;
+    if (this.props.error) {
+      errorMessage = (
+        <div className="message message-error">{this.props.error}</div>
+      );
+    }
+    return (
+      <form
+        className="add-dish-form"
+        onSubmit={this.props.handleSubmit((values, dispatch) =>
+          this.onSubmit(values, dispatch)
+        )}
+      >
+        {successMessage}
+        {errorMessage}
+        <fieldset>
+          <legend>Add dish form</legend>
+          <label htmlFor="name">Dish Name</label>
+          <Field name="name" type="text" component="input" />
+          <label htmlFor="rating">Rating</label>
+          <Field name="rating" type="text" component="input" />
+          <label htmlFor="description">Review</label>
+          <Field name="description" type="text" component="input" />
+          <button type="button" onClick={() => this.onCancelClick()}>
+            Cancel
+          </button>
+          <button type="submit">Add</button>
+        </fieldset>
+      </form>
+    );
+  }
 }
 export default reduxForm({
-    form: 'adddish'
+  form: "adddish"
 })(AddDishForm);
--- src/components/add-dish-review-form.js
+++ src/components/add-dish-review-form.js
@@ -1,35 +1,45 @@
-import React from 'react';
-import {reduxForm, Field} from 'redux-form';
-import { addDishReview } from '../actions';
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+import { addDishReview } from "../actions";
 
-export  class AddDishReview extends React.Component{
-    onSubmit(values, dispatch) {
-        const {restaurantId, dishId} = this.props.match.params;
-
-        dispatch(addDishReview(restaurantId, dishId, values));
-        this.props.history.push(`/restaurants/${restaurantId}/dishes/${dishId}/reviews`);
-    }
-    render() {
-        return(
-            <div>
-                <h3>Dish name</h3>
-                <form className="add-review-form" onSubmit={this.props.handleSubmit((values, dispatch) => this.onSubmit(values, dispatch))}>
-                    <fieldset>
-                        <legend>Add review form</legend>
-                        <label htmlFor="rating">Rating</label>
-                        <Field name="rating" type="text" component="input" />
-                        <label htmlFor="description">Review</label>
-                        <Field name="description" type="text" component="input" />
-                        <button>Cancel</button>
-                        <button type="submit">Post</button>
-                    </fieldset>     
-                </form>
-            </div>
-            
-        )
-    }
-    
+export class AddDishReview extends React.Component {
+  onSubmit(values, dispatch) {
+    const { restaurantId, dishId } = this.props.match.params;
+    dispatch(addDishReview(restaurantId, dishId, values));
+    this.props.history.push(
+      `/restaurants/${restaurantId}/dishes/${dishId}/reviews`
+    );
+  }
+  onCancelClick() {
+    const { restaurantId, dishId } = this.props.match.params;
+    this.props.history.push(`/restaurants/${restaurantId}/dishes`);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Dish name</h3>
+        <form
+          className="add-review-form"
+          onSubmit={this.props.handleSubmit((values, dispatch) =>
+            this.onSubmit(values, dispatch)
+          )}
+        >
+          <fieldset>
+            <legend>Add review form</legend>
+            <label htmlFor="rating">Rating</label>
+            <Field name="rating" type="text" component="input" />
+            <label htmlFor="description">Review</label>
+            <Field name="description" type="text" component="input" />
+            <button type="button" onClick={() => this.onCancelClick()}>
+              Cancel
+            </button>
+            <button type="submit">Post</button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
 }
 export default reduxForm({
-    form: 'addreview'
+  form: "addreview"
 })(AddDishReview);
