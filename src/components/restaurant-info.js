583a0bb04c10cd381654e29ebfb14adfb9d090ac
rk-codes <ping.you.back@gmail.com>
2018-03-22 00:36:21 -0700

    wrap withRouter and handle edit click

--- src/components/restaurant-info.js
+++ src/components/restaurant-info.js
@@ -1,47 +1,50 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-
-import {deleteRestaurant, editRestaurant} from '../actions';
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { deleteRestaurant, editRestaurant } from "../actions";
 
 class RestaurantInfo extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-    onDelete(){
-        console.log("Delete Clicked");
-        this.props.deleteRestaurant(this.props.id);
-    }
-    onEdit() {
-        console.log("Edit Clicked");
-        this.props.editRestaurant();
-    }
-    render() {
-        return(
-            <div className="restaurant-info">
-                <Link to= {`/restaurants/${this.props.id}/dishes`}>Restaurant Name: {this.props.name}</Link>
-                <p>Location: {this.props.location}</p>
-                <span>Cuisine: {this.props.cuisine}</span>
-                <p>Dishes Count: {this.props.dishIds.length}</p>
-                <button onClick={() => this.onEdit()}>Edit</button>
-                <button onClick={() => this.onDelete()}>Delete</button>
-            </div>
-        )
-    }
-   
+  // constructor(props) {
+  //     super(props);
+  // }
+  onDelete() {
+    console.log("Delete Clicked");
+    this.props.deleteRestaurant(this.props.id);
+  }
+  onEdit() {
+    console.log("Edit Clicked");
+    this.props.history.push("/editrestaurant");
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="restaurant-info">
+        <Link to={`/restaurants/${this.props.id}/dishes`}>
+          Restaurant Name: {this.props.name}
+        </Link>
+        <p>Location: {this.props.restLocation}</p>
+        <span>Cuisine: {this.props.cuisine}</span>
+        <p>Dishes Count: {this.props.dishIds.length}</p>
+        <button onClick={() => this.onEdit()}>Edit</button>
+        <button onClick={() => this.onDelete()}>Delete</button>
+      </div>
+    );
+  }
 }
-const mapStateToProps = (state) => {
-    return{
-        restaurants: state.restaurant.restaurants
-    }
-}
-const mapDispatchToProps = (dispatch) => {
-    return({
-        deleteRestaurant: (restaurantId) => dispatch(deleteRestaurant(restaurantId)),
-        editRestaurant: () => dispatch(editRestaurant())
-    })
-   
-}
-export default connect(mapStateToProps, mapDispatchToProps)(RestaurantInfo);
+const mapStateToProps = state => {
+  return {
+    restaurants: state.restaurant.restaurants
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteRestaurant: restaurantId => dispatch(deleteRestaurant(restaurantId)),
+    editRestaurant: () => dispatch(editRestaurant())
+  };
+};
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(RestaurantInfo)
+);
 
 //Delete clicked -> dispatch action to delete the restaurant -> Redirect to the restaurants list page
