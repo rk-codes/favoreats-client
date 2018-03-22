317b5743508d0e0394205db29bb75c418ac7bafb
rk-codes <ping.you.back@gmail.com>
2018-03-21 17:00:38 -0700

    modify normalize review id

--- src/utils/normalizer.js
+++ src/utils/normalizer.js
@@ -1,33 +1,33 @@
 export function normalizeRestaurant(restaurant) {
-    return{
-        [restaurant.id]: {
-            id: restaurant.id,
-            name: restaurant.name,
-            location: restaurant.location,
-            cuisine: restaurant.cuisine,
-            dishIds: restaurant.dishes
-        }
+  return {
+    [restaurant.id]: {
+      id: restaurant.id,
+      name: restaurant.name,
+      location: restaurant.location,
+      cuisine: restaurant.cuisine,
+      dishIds: restaurant.dishes
     }
+  };
 }
 
 export function normalizeDish(dish) {
-    return {
-        [dish.id]: {
-            id: dish.id,
-            name: dish.name,
-            reviewIds: dish.reviews
-        }
+  return {
+    [dish.id]: {
+      id: dish.id,
+      name: dish.name,
+      reviewIds: dish.reviews
     }
+  };
 }
 export function normalizeReview(review) {
-    return {
-        [review._id]: {
-            id: review._id,
-            reviewDate: review.reviewDate,
-            rating: review.rating,
-            description: review.description
-        }
+  return {
+    [review.id]: {
+      id: review.id,
+      reviewDate: review.reviewDate,
+      rating: review.rating,
+      description: review.description
     }
+  };
 }
 /*
     return{
@@ -40,5 +40,4 @@ export function normalizeReview(review) {
     dishes attribute behave the same way as above
 */
 
-//restuarant - mongoose document 
-
+//restuarant - mongoose document
