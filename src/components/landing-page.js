f225a6a3db18a05cde74cfe5d68081cec901aeb1
rk-codes <ping.you.back@gmail.com>
2018-03-27 11:21:02 -0700

    modify intro section

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -14,7 +14,6 @@ export function LandingPage(props) {
     <section className="landing-section">
       <div className="intro-box">
         <p>
-          <h3>favorEats</h3>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
