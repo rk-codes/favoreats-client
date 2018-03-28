ba57aff090b8de76fa93d541fdbac72b13c51681
rk-codes <ping.you.back@gmail.com>
2018-03-27 17:46:12 -0700

    add icon for form error

--- src/components/input.js
+++ src/components/input.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React from "react";
+import "./input.css";
 
 export default class Input extends React.Component {
-    componentDidUpdate(prevProps) {
-        if (!prevProps.meta.active && this.props.meta.active) {
-            this.input.focus();
-        }
+  componentDidUpdate(prevProps) {
+    if (!prevProps.meta.active && this.props.meta.active) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    let error;
+    if (this.props.meta.touched && this.props.meta.error) {
+      error = (
+        <span className="form-error">
+          <i className="fa fa-exclamation-triangle" /> {this.props.meta.error}
+        </span>
+      );
     }
 
-    render() {
-        let error;
-        if (this.props.meta.touched && this.props.meta.error) {
-            error = <div className="form-error">{this.props.meta.error}</div>;
-        }
-
-        let warning;
-        if (this.props.meta.touched && this.props.meta.warning) {
-            warning = (
-                <div className="form-warning">{this.props.meta.warning}</div>
-            );
-        }
-
-        return (
-            <div className="form-input">
-                <label htmlFor={this.props.input.name}>
-                    {this.props.label}
-                    {error}
-                    {warning}
-                </label>
-                <input
-                    {...this.props.input}
-                    id={this.props.input.name}
-                    type={this.props.type}
-                    ref={input => (this.input = input)}
-                />
-                {error}
-                {warning}
-            </div>
-        );
+    let warning;
+    if (this.props.meta.touched && this.props.meta.warning) {
+      warning = <span className="form-warning">{this.props.meta.warning}</span>;
     }
+
+    return (
+      <div className="form-input">
+        <label htmlFor={this.props.input.name}>
+          {this.props.label}
+          {error}
+          {warning}
+        </label>
+        <input
+          {...this.props.input}
+          id={this.props.input.name}
+          type={this.props.type}
+          ref={input => (this.input = input)}
+        />
+        {/* {error} */}
+        {warning}
+      </div>
+    );
+  }
 }
