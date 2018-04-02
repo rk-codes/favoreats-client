686f3f48672c4bb03e89200b713a2d625e771fa0
rk-codes <ping.you.back@gmail.com>
2018-04-01 17:29:09 -0700

    modify styling and message

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -12,15 +12,12 @@ export function LandingPage(props) {
     <section className="landing-section">
       <div className="intro-box">
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum
+          Finding a good place to eat is hard enough. Keeping track of them,
+          even harder. Fret no more!
+        </p>
+        <p>
+          Share your dining experience with us and we'll track your hits and
+          misses for you.
         </p>
       </div>
       <div className="about-outer">
@@ -28,7 +25,7 @@ export function LandingPage(props) {
           <div>
             <i className="fa fa-cutlery" aria-hidden="true" />
             <h4>Add restaurants</h4>
-            <p>Add restaurants that you've visited or are planning to</p>
+            <p>Add restaurants that you've visited or are planning to.</p>
           </div>
           <div>
             <i className="fa fa-comments-o" aria-hidden="true" />
