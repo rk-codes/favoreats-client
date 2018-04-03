eb6ca253a9800bb82861fb5b4ab82cba3448cc4d
rk-codes <ping.you.back@gmail.com>
2018-04-03 14:24:08 -0700

    change api base url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,10 @@
 // const env = runtimeEnv();
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://stark-lake-77252.herokuapp.com";
+// || "http://localhost:8080";
+
 // export const PORT = env.PORT || 8080;
 
 // export default {
