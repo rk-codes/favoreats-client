449cb862402cecada4229e372042203b3ec83f59
rk-codes <ping.you.back@gmail.com>
2018-04-03 11:29:32 -0700

    modify base url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8080,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-      "http://localhost:8080"
-  };
+  PORT: process.env.PORT || 8080,
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://stark-lake-77252.herokuapp.com"
+  // || "http://localhost:8080"
+};
