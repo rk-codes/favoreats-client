384b73e32f3754a8af3ea9e313c5b170e16d6b25
rk-codes <ping.you.back@gmail.com>
2018-04-03 14:54:38 -0700

    add localhost

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@
 
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://stark-lake-77252.herokuapp.com";
-// || "http://localhost:8080";
+  "https://stark-lake-77252.herokuapp.com" ||
+  "http://localhost:8080";
 
 // export const PORT = env.PORT || 8080;
 
