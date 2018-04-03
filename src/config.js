aa3fa50bc42ff75e9fdb9686692745ff25dd3ada
rk-codes <ping.you.back@gmail.com>
2018-04-03 13:16:28 -0700

    remove runtimeenv

--- src/config.js
+++ src/config.js
@@ -1,13 +1,11 @@
-import runtimeEnv from "@mars/heroku-js-runtime-env";
-const env = runtimeEnv();
+// import runtimeEnv from "@mars/heroku-js-runtime-env";
+// const env = runtimeEnv();
 
 export const API_BASE_URL =
-  env.REACT_APP_API_BASE_URL || "http://localhost:8080";
-export const PORT = env.PORT || 8080;
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+// export const PORT = env.PORT || 8080;
 
 // export default {
 //   PORT: env.PORT || 8080,
 //   API_BASE_URL: env.REACT_APP_API_BASE_URL || "http://localhost:8080"
 // };
-
-//commit
