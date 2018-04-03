77b66ee5c2e1e21ca2b41f9805e1060ba91d3be4
rk-codes <ping.you.back@gmail.com>
2018-04-03 12:24:47 -0700

    use es6 for export

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
 import runtimeEnv from "@mars/heroku-js-runtime-env";
 const env = runtimeEnv();
-module.exports = {
-  PORT: env.PORT || 8080,
-  API_BASE_URL: env.REACT_APP_API_BASE_URL || "http://localhost:8080"
-};
+
+export const API_BASE_URL =
+  env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+export const PORT = env.PORT || 8080;
+
+// export default {
+//   PORT: env.PORT || 8080,
+//   API_BASE_URL: env.REACT_APP_API_BASE_URL || "http://localhost:8080"
+// };
