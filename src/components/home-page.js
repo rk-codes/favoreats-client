56c6453ba24f24df477dea1ed0b453ba2eb353fb
rk-codes <ping.you.back@gmail.com>
2018-07-24 18:03:52 -0700

    add new user message

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -1,11 +1,39 @@
 import React from "react";
 
 import RestaurantsList from "../containers/restaurants-list";
+import { connect } from "react-redux";
 
-export default function HomePage() {
-  return (
-    <div>
-      <RestaurantsList />
-    </div>
-  );
+import { fetchAllRestaurants } from "../actions";
+
+export class HomePage extends React.Component {
+  componentDidMount() {
+    this.props.fetchAllRestaurants();
+  }
+
+  render() {
+    console.log("HOME");
+    if (Object.getOwnPropertyNames(this.props.restaurants).length === 0) {
+      console.log("home empty");
+      return <h3>Welcome! Start adding restaurants</h3>;
+    } else {
+      return (
+        <div>
+          <RestaurantsList />
+        </div>
+      );
+    }
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    restaurants: state.restaurant.restaurants
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAllRestaurants: () => dispatch(fetchAllRestaurants())
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
